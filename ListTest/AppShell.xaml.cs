f87b405ccf813d0dc36a9aac54847dfea3fd9023
Ulrich Trampe <trampe@picobit.de>
2022-11-30 08:37:41 +0100

    Scale property is irnored

--- ListTest/AppShell.xaml.cs
+++ ListTest/AppShell.xaml.cs
@@ -4,7 +4,18 @@ public partial class AppShell : Shell
 {
 	public AppShell()
 	{
+        Scale = 0.7;
+
 		InitializeComponent();
+
+        Scale = 0.7;
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        Scale = 0.7;
+    }
 }
 
